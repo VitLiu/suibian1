fd4205754e20dc228ea9a3bea836b323b0856715
583298411@qq.com <583298411@qq.com>
2018-01-02 17:12:09 +0800

    呵呵呵呵呵呵呵呵呵呵

---
+++ src/ZZ1/Demo2.java
@@ -0,0 +1,34 @@
+package ZZ1;
+
+import java.util.Scanner;
+
+public class Demo2 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+Scanner sc=new Scanner(System.in);
+System.out.println("???");
+System.out.println("???");
+System.out.println("???");
+System.out.println("???");
+System.out.println("???");
+System.out.println("???");
+System.out.println("???");
+System.out.println("???");
+System.out.println("???");
+System.out.println("???");
+System.out.println("???");
+System.out.println("???");
+System.out.println("???");
+System.out.println("???");
+System.out.println("???");
+int k=sc.nextInt();
+  
+System.out.println(k);
+
+	}
+public static int nextLnt(){
+return 0;
+	}
+
+}
